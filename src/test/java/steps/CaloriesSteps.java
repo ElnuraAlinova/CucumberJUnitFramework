d3e3f1297666800fecfea87ff8533ed5d28b55a2
ElnuraAlinova <alinova94@gmail.com>
2024-05-06 11:42:53 -0500

    Made some changes

--- src/test/java/steps/CaloriesSteps.java
+++ src/test/java/steps/CaloriesSteps.java
@@ -1,58 +1,39 @@
 package steps;
 
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
+import io.cucumber.java.en.*;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import utilities.Driver;
+
 
 public class CaloriesSteps {
+
+    WebDriver driver = Driver.getDriver();
     @Given("user goes to {string}")
-    public void user_goes_to(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void user_goes_to(String url) {
+        driver.get(url);
+
+
     }
     @Given("user enters {int} calories")
-    public void user_enters_calories(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void user_enters_calories(Integer amountOfCalories) {
+        System.out.println("user entered calories: " + amountOfCalories);
+
     }
     @Then("user selects {int} meals")
-    public void user_selects_meals(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void user_selects_meals(Integer amountOfMeals) {
+        System.out.println("user selected meals: "+ amountOfMeals);
+
     }
     @Then("user clicks on generate")
     public void user_clicks_on_generate() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("user clicked on generate");
+
     }
     @Then("verify {int} meals are generated")
-    public void verify_meals_are_generated(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Given("user goes to {string}")
-    public void user_goes_to(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Given("user enters {int} calories")
-    public void user_enters_calories(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("user selects {int} meals")
-    public void user_selects_meals(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("user clicks on generate")
-    public void user_clicks_on_generate() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("verify {int} meals are generated")
-    public void verify_meals_are_generated(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void verify_meals_are_generated(Integer amountOfMeals) {
+        Assert.assertTrue(amountOfMeals == 4);
+
     }
 
 }
